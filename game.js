adf81e2e0c8dfbf9f3747373a02fbabc387336c5
uesclei1988 <118620983+uesclei1988@users.noreply.github.com>
2025-11-16 21:05:25 -0300

    Add basic styles for body and canvas elements

---
+++ game.js
@@ -0,0 +1,12 @@
+body {
+    background: #111;
+    color: #fff;
+    font-family: Arial;
+    text-align: center;
+}
+
+canvas {
+    background: #000;
+    border: 3px solid #555;
+    margin-top: 20px;
+}
